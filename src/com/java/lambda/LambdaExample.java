7ca2063e05289dd6aeaa92e68dcb4e14325fa5f6
prabhat <kapkotiprabhat@gmail.com>
2025-12-23 18:09:41 +0530

    basic of lambda expression

--- src/com/java/lambda/LambdaExample.java
+++ src/com/java/lambda/LambdaExample.java
@@ -1,5 +1,40 @@
 package com.java.lambda;
 
-public class LambdaExample {
+interface Shape{
+    void draw();
+}
 
+class Rectangle implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Rectangle class : draw method ");
+    }
+}
+
+class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Square class : draw method ");
+    }
+}
+
+
+class  Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Circle class : draw method ") ;
+    }
+}
+
+public class LambdaExample {
+    public static void main(String[] args) {
+
+        String str = "Hello";
+
+        Shape rectangle =()-> System.out.println("Rectangle Class: draw() method");
+
+        rectangle.draw();
+
+    }
 }
